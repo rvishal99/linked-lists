31a25b3b7734a282da221c84e53e280a855af350
rvishal99 <90997842+rvishal99@users.noreply.github.com>
2022-04-10 22:44:30 +0530

    Add files via upload

---
+++ 9-circular-LL.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+bool isCircularLL(Node* &head)
+{
+    if(head==NULL)
+    {
+        return true;
+
+    }
+    Node*temp = head->next;
+    while(temp!=NULL && temp!=head)
+    {
+       temp = temp->next;
+    }
+    if(temp==head)
+    {
+        return true;
+    } 
+       return false;
+}
+
+int main()
+{
+
+    return 0;
+}
